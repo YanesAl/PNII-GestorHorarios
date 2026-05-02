445588e14c37258fdc45612e7cfd2ea2cbfa42c8
YanesM1 <myanes@unbosque.edu.co>
2026-05-02 12:17:49 -0500

    feat(GestorHorarios):creación del proyecto GestorHorarios

---
+++ src/main/java/co/edu/unbosque/PNII_GestorHorarios/PniiGestorHorariosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.PNII_GestorHorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PniiGestorHorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PniiGestorHorariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/PNII_GestorHorarios/PniiGestorHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.PNII_GestorHorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PniiGestorHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
